f77f729f5f9b92e643162ce0b6f3983225c7bbb8
iceBear67 <icebear67@sfclub.cc>
2021-07-04 11:23:16 +0800

    try 2 fix CI

--- src/test/java/TestSpigot.java
+++ src/test/java/TestSpigot.java
@@ -30,7 +30,9 @@ public class TestSpigot {
         Files.copy(OUR_JAR.toPath(),new File(PLUGIN_DIR,"ManiHunt.jar").toPath());
         File PAPI = new File("./spigot/plugins/PAPI.jar");
         HttpRequest.get("https://github.com/PlaceholderAPI/PlaceholderAPI/releases/download/2.10.9/PlaceholderAPI-2.10.9.jar").receive(PAPI);
-        boolean b = new ProcessBuilder().directory(SPIGOT_ROOT).command("java","-jar","spigot.jar","-W",new File("./ci").toPath().toString()).start().waitFor(65, TimeUnit.SECONDS);
+        Process b = new ProcessBuilder().directory(SPIGOT_ROOT).command("java","-jar","spigot.jar","-W",new File("./ci").toPath().toString()).start();
+        b.waitFor(65, TimeUnit.SECONDS);
+        b.destroy();
       
         boolean a=Files.readAllLines(new File(SPIGOT_ROOT,"logs/latest.log").toPath()).stream().anyMatch(e->e.contains("ManHunt Started! We're waiting for more players."))?false:true;
         Files.copy(new File(SPIGOT_ROOT,"logs/latest.log").toPath(),new File("./build/libs/CI.log").toPath());
